f96a4012b6141b1e5ae60fd9079228dbb9766bf9
henrynguyen <henrylknguyen@gmail.com>
2024-11-26 10:26:20 +1100

    Update with lesson 020302 - Implement image switch to display image logo or logo

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.imageSwitch.setOnCheckedChangeListener { _, isChecked ->
+            //binding.imageView.visibility = if (isChecked) View.VISIBLE else View.GONE
+            if (isChecked) {
+                binding.imageView.setImageResource(R.drawable.olive_branch)
+                binding.textView.text = "Olive Branch"
+            }
+            else {
+                binding.imageView.setImageResource(R.drawable.logo)
+                binding.textView.text = "Logo"
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
