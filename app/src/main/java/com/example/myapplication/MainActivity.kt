6671e0f1cf640e13f9d520b621bdea9af0a0d86b
henrynguyen <henrylknguyen@gmail.com>
2024-11-26 00:29:36 +1100

    Update with lesson 02020 - Implement three TextLayout fields, firstname, colour and snack and display the result to Toast and TextView

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submitButton.setOnClickListener {
+            val firstName = binding.firstName.text.toString().trim()
+            val favouriteColour = binding.favouriteColour.text.toString().trim()
+            val favouriteSnack = binding.favouriteSnack.text.toString().trim()
+
+            val message = "Hello $firstName. Your favourite colour is $favouriteColour and your favourite snack is $favouriteSnack"
+
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            binding.textView.text=message
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
